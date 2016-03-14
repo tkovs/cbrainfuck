15b12d24d2daf8261a9229bded146351ff595125
tkovs <victor.rdg@hotmail.com>
2016-03-13 21:41:57 -0300

    Details

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 
 int main(int argc, char ** argv) {
 	char *s;
-	s = interpreter(argv[1], argc>2?argv[2]:"/dev/stdin");
+	s = interpreter(argv[1], (argc > 2 ? argv[2]:"/dev/stdin"));
 
-	if (_ERRORS_ > 0) {
+	if (_ERRORS_) {
 		fprintf(stderr, "%s\n", _MESSAGE_);
 		return 1;
 	} else {
