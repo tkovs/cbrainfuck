4a4de99a3332b20dca3700d2c69eb113a5a7ad45
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-01-06 17:24:47 -0300

    Changes the variable size to read comments
    Move function (... to function(

--- source/cbrainfuck.c
+++ source/cbrainfuck.c
@@ -3,57 +3,59 @@
 #include <string.h>
 #include "../include/cbrainfuck.h"
 
-int open_files (FILE** code_file, char* code_file_name, FILE** input_file, char* input_file_name)
+#define _MAXLINELENGTH_ 256
+
+int open_files(FILE** code_file, char* code_file_name, FILE** input_file, char* input_file_name)
 {
-    (*code_file) = fopen (code_file_name, "r");
+    (*code_file) = fopen(code_file_name, "r");
 
     if ((*code_file) != NULL)
     {
-        (*input_file) = fopen (input_file_name, "r");
+        (*input_file) = fopen(input_file_name, "r");
         if ((*input_file) == NULL)
         {
             fclose((*code_file));
-            report ("Couldn't open input file");
+            report("Couldn't open input file");
             return 1;
         }
     }
     else
     {
-        report ("Couldn't open code file");
+        report("Couldn't open code file");
         return 1;
     }
 
     return 0;
 }
 
-void initialize (List *list)
+void initialize(List *list)
 {
     list->value = 0;
     list->next = NULL;
     list->prev = NULL;
 }
 
-void create (List *list)
+void create(List *list)
 {
     List *newlist;
 
-    newlist = (List *) malloc (sizeof (List));
-    initialize (newlist);
+    newlist = (List *) malloc(sizeof(List));
+    initialize(newlist);
 
     newlist->prev = list;
     list->next = newlist;
 }
 
-void liberate (List *list)
+void liberate(List *list)
 {
     if (list != NULL)
     {
-        liberate (list->next);
-        free (list);
+        liberate(list->next);
+        free(list);
     }
 }
 
-char* interpreter (char* code_file_name, char* input_file_name)
+char* interpreter(char* code_file_name, char* input_file_name)
 {
     _ERRORS_ = 0;
     free(_MESSAGE_);
@@ -64,7 +66,7 @@ char* interpreter (char* code_file_name, char* input_file_name)
     int *brackets = NULL;
     List *fuckinglist = NULL;
     char *result = NULL, c;
-    char temp[64]; // Read string of the comments
+    char *comment = NULL;
     short int countscan = 0; // Number of scans
     short int countpoint = 1; // Number of '.'
     short int length = 0; // Number of bracket '['
@@ -74,21 +76,24 @@ char* interpreter (char* code_file_name, char* input_file_name)
         return NULL;
     }
 
-    result = (char *) malloc (sizeof (char));
-    result[0] = '\0';
-    brackets = (int *) malloc (sizeof (int));
-    fuckinglist = (List *) malloc (sizeof (List));
-    initialize (fuckinglist);
+    // Initializes the "comment" variable using the constant _MAXLINELENGTH_
+    comment = (char *) malloc(sizeof(char) * _MAXLINELENGTH_);
 
-    while((c = fgetc (fcode)) != EOF)
+    result = (char *) malloc(sizeof(char));
+    result[0] = '\0';
+    brackets = (int *) malloc(sizeof(int));
+    fuckinglist = (List *) malloc(sizeof(List));
+    initialize(fuckinglist);
+
+    while((c = fgetc(fcode)) != EOF)
     {
         countscan++;
 
         switch (c)
         {
             case COMMENT:
-                fgets (temp, 64, fcode);
-                countscan += strlen (temp);
+                fgets(comment, 64, fcode);
+                countscan += strlen(comment);
                 break;
 
             case INCREMENT:
@@ -102,7 +107,7 @@ char* interpreter (char* code_file_name, char* input_file_name)
             case NEXT:
                 if (fuckinglist->next == NULL)
                 {
-                    create (fuckinglist);
+                    create(fuckinglist);
                 }
 
                 fuckinglist = fuckinglist->next;
@@ -122,12 +127,12 @@ char* interpreter (char* code_file_name, char* input_file_name)
                 if (fuckinglist->value == 0) break;
                 result[countpoint-1] = fuckinglist->value;
                 countpoint++;
-                result = (char *) realloc (result, countpoint * sizeof (char));
+                result = (char *) realloc(result, countpoint * sizeof(char));
                 result[countpoint-1] = '\0';
                 break;
 
             case SCAN:
-                fscanf (finput, "%c", &fuckinglist->value);
+                fscanf(finput, "%c", &fuckinglist->value);
                 break;
 
             case OPENLOOP:
@@ -135,7 +140,7 @@ char* interpreter (char* code_file_name, char* input_file_name)
                     int nested = 1;
                     while (nested) {
                         char c1;
-                        fscanf (fcode, "%c", &c1);
+                        fscanf(fcode, "%c", &c1);
                         countscan++;
                         if (c1 == '[') nested++;
                         if (c1 == ']') nested--;
@@ -144,20 +149,20 @@ char* interpreter (char* code_file_name, char* input_file_name)
                 }
 
                 length++;
-                brackets = (int *) realloc (brackets, length * sizeof (int));
+                brackets = (int *) realloc(brackets, length * sizeof(int));
                 brackets[length-1] = countscan;
                 break;
 
             case CLOSELOOP:
                 if (fuckinglist->value != 0)
                 {
-                    fseek (fcode, brackets[length-1], SEEK_SET);
+                    fseek(fcode, brackets[length-1], SEEK_SET);
                     countscan = brackets[length-1];
                 }
                 else
                 {
                     length--;
-                    brackets = (int *) realloc (brackets, length * sizeof (int));
+                    brackets = (int *) realloc(brackets, length * sizeof(int));
                 }
                 break;
         }
@@ -166,16 +171,16 @@ char* interpreter (char* code_file_name, char* input_file_name)
 
     while (fuckinglist->prev != NULL)
         fuckinglist = fuckinglist->prev;
-    liberate (fuckinglist);
+    liberate(fuckinglist);
 
-    fclose (fcode);
-    fclose (finput);
+    fclose(fcode);
+    fclose(finput);
 
     result[countpoint-1] = '\0';
     return result;
 }
 
-void report (char *msg) {
+void report(char *msg) {
     _ERRORS_ = 1;
     _MESSAGE_ = msg;
 }
